6e248ed7ab84575287f61ba6f470a20f93fba1e2
Raj Ramsaroop <raj@bewareofraj.com>
2014-11-06 15:47:31 -0500

    retrieve json string in async task

--- src/com/bewareofraj/mytvtracker/api/RetrieveTraktJSONTask.java
+++ src/com/bewareofraj/mytvtracker/api/RetrieveTraktJSONTask.java
@@ -1,13 +1,48 @@
 package com.bewareofraj.mytvtracker.api;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.os.AsyncTask;
 
-public class RetrieveTraktJSONTask extends AsyncTask<String, Void, String> {
+public class RetrieveTraktJSONTask extends AsyncTask<String, Void, JSONObject> {
 
 	@Override
-	protected String doInBackground(String... queries) {
-		// TODO Auto-generated method stub
-		return null;
+	protected JSONObject doInBackground(String... queries) {
+		JSONObject result = null;
+		try {
+			URL url = new URL(queries[0]);
+			InputStream is = url.openStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			
+			StringBuilder jsonStringBuilder = new StringBuilder();
+    		String line;
+    		while ((line = br.readLine()) != null) {
+    			jsonStringBuilder.append(line);
+    		}
+
+    		br.close();
+    		is.close();
+    		
+    		result = new JSONObject(jsonStringBuilder.toString());
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 }
--- src/com/bewareofraj/mytvtracker/api/TraktApiHelper.java
+++ src/com/bewareofraj/mytvtracker/api/TraktApiHelper.java
@@ -1,9 +1,15 @@
 package com.bewareofraj.mytvtracker.api;
 
+import java.util.concurrent.ExecutionException;
+
+import org.json.JSONObject;
+
 public class TraktApiHelper {
 	
 	public static final String API_BASE_URL = "http://api.trakt.tv/";
 	public static final String API_METHOD_SHOW = "show/";
+	public static final String API_ARGUMENT_SHOW_SUMMARY = "summary";
+	public static final String API_FORMAT = ".json/";
 	
 	private String mApiKey = "";
 	
@@ -17,6 +23,23 @@ public class TraktApiHelper {
 	 * @return A Show object
 	 */
 	public Show getShow(String id) {
+		StringBuilder query = new StringBuilder();
+		query.append(API_BASE_URL);
+		query.append(API_METHOD_SHOW);
+		query.append(API_ARGUMENT_SHOW_SUMMARY);
+		query.append(API_FORMAT);
+		query.append(mApiKey);
+		query.append(id + "/");
+		try {
+			JSONObject result = new RetrieveTraktJSONTask().execute(query.toString()).get();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 
@@ -25,7 +48,7 @@ public class TraktApiHelper {
 	}
 
 	public void setApiKey(String mApiKey) {
-		this.mApiKey = mApiKey;
+		this.mApiKey = mApiKey + "/";
 	}
 
 }
