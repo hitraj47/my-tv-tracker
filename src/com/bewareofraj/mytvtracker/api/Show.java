39056e2bf2c6ac3b23eb96684e5582fa23e4c944
Raj Ramsaroop <raj@bewareofraj.com>
2014-11-06 14:23:20 -0500

    add api package

---
+++ src/com/bewareofraj/mytvtracker/api/Show.java
@@ -0,0 +1,114 @@
+package com.bewareofraj.mytvtracker.api;
+
+/**
+ * A Show class represents a TV show.
+ * @author Rajesh
+ *
+ */
+public class Show {
+	
+	private String mTitle;
+	private int mYear;
+	private String mFirstAired;
+	private String mCountry;
+	private String mOverview;
+	private String mStatus;
+	private String mNetwork;
+	private String mAirDay;
+	private String mAirTime;
+	private String mTvdbId;
+	private String mPosterUrl;
+	
+	public Show() {
+		
+	}
+
+	public String getTitle() {
+		return mTitle;
+	}
+
+	public void setTitle(String mTitle) {
+		this.mTitle = mTitle;
+	}
+
+	public int getYear() {
+		return mYear;
+	}
+
+	public void setYear(int mYear) {
+		this.mYear = mYear;
+	}
+
+	public String getFirstAired() {
+		return mFirstAired;
+	}
+
+	public void setFirstAired(String mFirstAired) {
+		this.mFirstAired = mFirstAired;
+	}
+
+	public String getCountry() {
+		return mCountry;
+	}
+
+	public void setCountry(String mCountry) {
+		this.mCountry = mCountry;
+	}
+
+	public String getOverview() {
+		return mOverview;
+	}
+
+	public void setOverview(String mOverview) {
+		this.mOverview = mOverview;
+	}
+
+	public String getStatus() {
+		return mStatus;
+	}
+
+	public void setStatus(String mStatus) {
+		this.mStatus = mStatus;
+	}
+
+	public String getNetwork() {
+		return mNetwork;
+	}
+
+	public void setNetwork(String mNetwork) {
+		this.mNetwork = mNetwork;
+	}
+
+	public String getAirDay() {
+		return mAirDay;
+	}
+
+	public void setAirDay(String mAirDay) {
+		this.mAirDay = mAirDay;
+	}
+
+	public String getAirTime() {
+		return mAirTime;
+	}
+
+	public void setAirTime(String mAirTime) {
+		this.mAirTime = mAirTime;
+	}
+
+	public String getTvdbId() {
+		return mTvdbId;
+	}
+
+	public void setTvdbId(String mTvdbId) {
+		this.mTvdbId = mTvdbId;
+	}
+
+	public String getPosterUrl() {
+		return mPosterUrl;
+	}
+
+	public void setPosterUrl(String mPosterUrl) {
+		this.mPosterUrl = mPosterUrl;
+	}
+
+}
