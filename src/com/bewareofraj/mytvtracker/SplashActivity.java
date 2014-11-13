e3113b24231caff6614705230103a41801147fd1
Raj Ramsaroop <raj@bewareofraj.com>
2014-11-13 03:05:16 -0500

    set splash background to white

--- src/com/bewareofraj/mytvtracker/SplashActivity.java
+++ src/com/bewareofraj/mytvtracker/SplashActivity.java
@@ -2,8 +2,6 @@ package com.bewareofraj.mytvtracker;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class SplashActivity extends Activity {
 
