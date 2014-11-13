bc225bc6175425b4bf96331baac08f35813aa819
Raj Ramsaroop <raj@bewareofraj.com>
2014-11-13 03:21:47 -0500

    change splash background color

--- src/com/bewareofraj/mytvtracker/SplashActivity.java
+++ src/com/bewareofraj/mytvtracker/SplashActivity.java
@@ -2,6 +2,7 @@ package com.bewareofraj.mytvtracker;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class SplashActivity extends Activity {
 
@@ -9,5 +10,6 @@ public class SplashActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
+		
 	}
 }
